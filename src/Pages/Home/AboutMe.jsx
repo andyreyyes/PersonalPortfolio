17de8efc219f2418d447ac6ee90b1a71a7591853
andyreyes <andrew.reyes52012@gmail.com>
2024-01-25 15:08:00 -0700

    fixed wording and spelling

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,8 +8,8 @@ export default function AboutMe() {
                 <div className="hero--section--content">
                     <p className="section--title">About</p>
                     <h1 className="skills-section--heading">About Me</h1>
-                    <p className="hero--section-description">While being a native of Tucson and currently a junior at the Universtiy of Arizona, I am a server at one of the oldest steak houses in Tucson. I am currently looking to pursue a coding Internship. </p>
-                    <p className="hero--section-description">I am pursing a coding Internship because I am excited to dive in and put everything I have learned to use. I'm ready to get to work and get the experience I need to be successful in my career. You can contact me at <b>andrew.reyes52012@gmail.com</b></p>
+                    <p className="hero--section-description">I am currently a junior at the University of Arizona and have a graduation date of 05/2025. I am seeking oppurtunities for a software engineering internship hoping to improve my skills and learn more from more experienced developers. </p>
+                    <p className="hero--section-description">In order to prepare myself for these oppurtunities, I have worked on several personal projects (which are listed below) to broaden my knowledge and sharpen my skills. You can contact me at <b>andrew.reyes52012@gmail.com</b></p>
                 </div>
             </div>
         </section>
